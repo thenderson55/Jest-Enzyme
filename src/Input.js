48159cd98951289b0b8124cbae0b7d7a8aa9d0b9
Thomas Henderson <tbhenderson55@yahoo.com>
2019-09-16 20:29:47 +0900

    useState mocked and tested as well as preventDefault

--- src/Input.js
+++ src/Input.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 function Input({ secretWord }) {
+  const [currentGuess, setCurrentGuess] = React.useState();
   return (
     <div data-test="component-input">
-      
+      <form>
+        <input
+          type="text"
+          data-test="input-box"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={e => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentGuess("");
+          }}
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
 Input.propTypes = {
   secretWord: PropTypes.string.isRequired
-}
+};
 
-export default Input
+export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,20 +1,49 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import { findByTestAttr, checkProps } from './testUtils'
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "./testUtils";
 
-import Input from './Input'
+import Input from "./Input";
 
-const setup = (secretWord='party') => {
-  return shallow(<Input secretWord={secretWord}/>)
-}
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
+};
 
-it('should render component without error', () => {
-  const wrapper = setup()
-  const component = findByTestAttr(wrapper, 'component-input')
-  expect(component.length).toBe(1)
-})
+it("should render component without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-input");
+  expect(component.length).toBe(1);
+});
 
-it('should receive the correct props', () => {
-  const expectedProps = { secretWord: 'party'}
-  checkProps(Input, expectedProps)
-})
+it("should receive the correct props", () => {
+  const expectedProps = { secretWord: "party" };
+  checkProps(Input, expectedProps);
+});
+
+describe("state controlled input field", () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  test("state updates with value of input box upon change", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+  test("when button is clicked and empty string is passed to setCurrentGuess", () => {
+    const button = findByTestAttr(wrapper, "submit-button");
+
+    button.simulate("click", {
+      preventDefault() {}
+    });
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
+});
